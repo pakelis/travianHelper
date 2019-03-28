c622e4ca7a25b3ec53575b45e9d4e53ac454b42f
mannypa <pakelis47@gmail.com>
2019-03-28 23:50:24 +0200

    initial commit

--- routes/api/servers.js
+++ routes/api/servers.js
@@ -11,19 +11,41 @@ const Server = require('../../models/Server')
 
 router.get('/servers', (req, res) => {
   let servers = Server.find()
+    //sort by server days
     .sort({'server.days': 1})
     .then(servers => {
       let stringList = []
       for (const item of servers) {
+        //Check if item is Not an number
         if (isNaN(item.server.days)) {
+          //get date from string
+          date = new Date(item.server.days)
+          year = date.getFullYear()
+          month =
+            date.getMonth() + 1 > 9
+              ? date.getMonth() + 1
+              : `0${date.getMonth() + 1}`
+          day = `${date.getDate()}` > 9 ? date.getDate() : `0${date.getDate()}`
+          item.server.days = `${year}-${month}-${day}`
           stringList.push(item)
         }
+
+        stringList.sort((a, b) => {
+          a = a
+            .toString()
+            .split('-')
+            .reverse()
+            .join('')
+          b = b
+            .toString()
+            .split('-')
+            .reverse()
+            .join('')
+          return a > b ? 1 : a < b ? -1 : 0
+        })
       }
       const sortedList = stringList.concat(servers)
-      let string = '02 Apr 2019'
-      let d = new Date(string)
-      return res.json(`${d.getFullYear()}/0${d.getMonth() + 1}/0${d.getDay()}`)
-      // return res.json(sortedList)
+      return res.json(sortedList)
     })
   // .then(servers => res.json(servers))
   // .catch(err => res.status(404).json({noserversfound: 'No servers found'}))
