2463bb268d08a32e6c2a7578babcb0d76a39f342
unknown <pakelis47@gmail.com>
2019-04-05 13:17:09 +0300

    initial commit

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-light text-dark mt-5 p-4 text-center">
+    <footer className="bg-white text-dark mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} travianHelper
     </footer>
   )
---
+++ client/src/components/serverlist/ServerList.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+class ServerList extends Component {
+  render() {
+    return <div />
+  }
+}
+export default ServerList
