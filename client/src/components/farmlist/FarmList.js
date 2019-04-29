86fff03daf4ae23ff5f9ae5865b06622ce604384
unknown <pakelis47@gmail.com>
2019-04-29 21:52:13 +0300

    initial commit

--- client/src/components/farmlist/FarmList.js
+++ client/src/components/farmlist/FarmList.js
@@ -3,17 +3,36 @@ import {ClipLoader} from 'react-spinners'
 import axios from 'axios'
 import FarmTable from './FarmTable'
 
+const urlRegex = RegExp(
+  /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+)
+const coordRegex = RegExp(/^[0-9 ()+-]+$/)
+
+const formValid = ({formErrors, ...rest}) => {
+  let valid = true
+
+  //if val.length greater then 0 put valid to false, cus there is an error then.
+  Object.values(formErrors).forEach(val => val.length > 0 && (valid = false))
+
+  //validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false)
+  })
+
+  return valid
+}
+
 class FarmList extends Component {
   state = {
     loading: false,
     players: [],
     unpublished: {
-      serverName: '',
-      x: '',
-      y: '',
-      minPop: '',
-      maxPop: '',
-      distance: '',
+      serverName: null,
+      x: null,
+      y: null,
+      minPop: null,
+      maxPop: null,
+      distance: null,
     },
     published: {
       serverName: '',
@@ -23,7 +42,33 @@ class FarmList extends Component {
       maxPop: '',
       distance: '',
     },
+    formErrors: {
+      serverName: '',
+      x: '',
+      y: '',
+      minPop: '',
+      maxPop: '',
+      distance: '',
+    },
     displayList: 0,
+  }
+
+  handleErrors = e => {
+    if (formValid(this.state)) {
+      console.log(`
+        Submitting Form
+        ServerName: ${this.state.unpublished.serverName}
+        x: ${this.state.unpublished.x}
+        y: ${this.state.unpublished.y}
+        minPop: ${this.state.unpublished.minPop}
+        maxPop: ${this.state.unpublished.maxPop}
+        distance: ${this.state.unpublished.distance}
+      `)
+    } else {
+      console.log(`
+        FORM INVALID - DISPLAY ERROR MESSAGE
+      `)
+    }
   }
 
   displayList = e => {
@@ -44,13 +89,54 @@ class FarmList extends Component {
   }
 
   onInputChange = e => {
-    this.setState({
-      // spreading unpublished state so i could use [e.target.name] : e.target.value
-      unpublished: {
-        ...this.state.unpublished,
-        [e.target.name]: e.target.value,
+    const {name, value} = e.target
+    let formErrors = {...this.state.formErrors}
+    console.log(formErrors.x)
+
+    switch (name) {
+      case 'serverName':
+        formErrors.serverName = urlRegex.test(value) ? '' : 'wrong server name'
+        break
+      case 'x':
+        formErrors.x = coordRegex.test(value)
+          ? ''
+          : 'cannot contain that symbol'
+        break
+      case 'y':
+        formErrors.y = coordRegex.test(value)
+          ? ''
+          : 'cannot contain that symbol'
+        break
+      case 'minPop':
+        formErrors.minPop = coordRegex.test(value)
+          ? ''
+          : 'cannot contain that symbol'
+        break
+      case 'maxPop':
+        formErrors.maxPop = coordRegex.test(value)
+          ? ''
+          : 'cannot contain that symbol'
+        break
+      case 'distance':
+        formErrors.distance = coordRegex.test(value)
+          ? ''
+          : 'cannot contain that symbol'
+        break
+      default:
+        break
+    }
+
+    this.setState(
+      {
+        // spreading unpublished state so i could use [e.target.name] : e.target.value
+        formErrors,
+        unpublished: {
+          ...this.state.unpublished,
+          [e.target.name]: e.target.value,
+        },
       },
-    })
+      () => console.log(this.state),
+    )
   }
 
   enableSpinner() {
@@ -60,6 +146,8 @@ class FarmList extends Component {
   }
 
   render() {
+    const {formErrors} = this.state
+
     let list = null
 
     if (this.state.displayList === 1 && this.state.loading === false) {
@@ -85,63 +173,127 @@ class FarmList extends Component {
               <div className="card-header text-center">Farm Finder</div>
               <div className="card-body">
                 <div className="form-row">
-                  <label className="control-label ml-1">Server name</label>
-                  <input
-                    type="text"
-                    className="form-control form-rounded"
-                    placeholder="ts2.travian.com"
-                    name="serverName"
-                    onChange={this.onInputChange}
-                  />
-                  <div className="form-group col-12 col-md-6 mt-3">
+                  <div className="form-group col-12 col-md-12">
+                    <label className="control-label ml-1">Server name</label>
+                    <input
+                      type="text"
+                      className={
+                        formErrors.serverName.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.serverName === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
+                      placeholder="Server Name e.g. ts2.travian.com"
+                      name="serverName"
+                      onChange={this.onInputChange}
+                    />
+                    {formErrors.serverName.length > 0 && (
+                      <div className="invalid-feedback">
+                        {formErrors.serverName}
+                      </div>
+                    )}
+                  </div>
+                  <div className="form-group col-12 col-md-6 ">
                     <label className="control-label ml-1">X coordinates</label>
                     <input
                       type="text"
-                      className="form-control form-rounded"
+                      className={
+                        formErrors.x.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.x === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
                       placeholder="X"
                       name="x"
                       onChange={this.onInputChange}
                     />
+                    {formErrors.x.length > 0 && (
+                      <div className="invalid-feedback">{formErrors.x}</div>
+                    )}
                   </div>
-                  <div className="form-group col-12 col-md-6 mt-3">
+                  <div className="form-group col-12 col-md-6 ">
                     <label className="control-label ml-1">Y coordinates</label>
                     <input
                       type="text"
-                      className="form-control form-rounded"
+                      className={
+                        formErrors.y.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.y === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
                       placeholder="Y"
                       name="y"
                       onChange={this.onInputChange}
                     />
+                    {formErrors.y.length > 0 && (
+                      <div className="invalid-feedback">{formErrors.y}</div>
+                    )}
                   </div>
                   <div className="form-group col-12 col-md-6">
                     <label className="control-label ml-1">Min population</label>
                     <input
                       type="text"
-                      className="form-control form-rounded"
+                      className={
+                        formErrors.minPop.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.minPop === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
                       placeholder="Min"
                       name="minPop"
                       onChange={this.onInputChange}
                     />
+                    {formErrors.minPop.length > 0 && (
+                      <div className="invalid-feedback">
+                        {formErrors.minPop}
+                      </div>
+                    )}
                   </div>
                   <div className="form-group col-12 col-md-6">
                     <label className="control-label ml-1">Max population</label>
                     <input
                       type="text"
-                      className="form-control form-rounded"
+                      className={
+                        formErrors.maxPop.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.maxPop === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
                       placeholder="Max"
                       name="maxPop"
                       onChange={this.onInputChange}
                     />
+                    {formErrors.maxPop.length > 0 && (
+                      <div className="invalid-feedback">
+                        {formErrors.maxPop}
+                      </div>
+                    )}
                   </div>
-                  <div className="form-group col-12 col-md-6 ">
+                  <div className="form-group col-12 col-md-12 ">
                     <label className="control-label ml-1">Distance</label>
                     <input
                       type="text"
-                      className="form-control form-rounded"
+                      className={
+                        formErrors.distance.length > 0
+                          ? `form-control form-rounded is-invalid`
+                          : this.state.unpublished.distance === null
+                          ? `form-control form-rounded`
+                          : `form-control form-rounded is-valid`
+                      }
                       placeholder=""
                       name="distance"
                       onChange={this.onInputChange}
                     />
+                    {formErrors.distance.length > 0 && (
+                      <div className="invalid-feedback">
+                        {formErrors.distance}
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
